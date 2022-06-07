1e896344c57fc5b3cad7733c5fc60beeacd408c0
Danny Komen <kipropd2@gmail.com>
2022-06-07 10:41:08 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * print the alphabets 10 times
+ */
+void print_alphabet_x10()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		char alp;
+
+		for (alp = 'a'; alp <= 'z' ; alp++)
+		{
+			_putchar(alp);
+		}
+	}
+	_putchar('\n');
+}
