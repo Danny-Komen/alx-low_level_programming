f22b55eb29183fc6e5b0ce6f9c1a729d07b5c283
Danny Komen <kipropd2@gmail.com>
2022-06-08 09:28:09 -0700

    9

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,3 @@
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(char c);
-int _isalpha(char c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-int add(int, int);
-void print_to_98(int n);
+void times_table(void);
