06680ac166730c0c6deb966b6ec7f379eb842c40
Danny Komen <kipropd2@gmail.com>
2022-06-09 04:51:11 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if its a digit
+ * @c: a character type parameter
+ * Return: 1 or 0
+ */
+int _isdigit(char c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 int _putchar(char c);
 void print_alphabet(void);
 void times_table(void);
-int _isupper(char c);
+int _isupper(int c);
+int _isdigit(int c);
