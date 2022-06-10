45cd28ae56c83dc773ea28de5c42553d6ab20693
Danny Komen <kipropd2@gmail.com>
2022-06-09 23:22:57 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_numbers - print numbers from 0 to 9
+ * print_most_numbers - print numbers from 0 to 9
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char i;
 
