22b55ccab640f5d472b27b870513ee9d31463bae
Danny Komen <kipropd2@gmail.com>
2022-06-09 23:04:30 -0700

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
- * print_numbers - prints zero to 9
- * Return: void
+ * print_numbers - print numbers from 0 to 9
  */
 void print_numbers(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i < 10; i++)
+	for (i  = 48; i < 58; i++)
 	{
 		_putchar(i);
 	}
-
 	_putchar('\n');
 }
