f9390b0b0bde9f0ec18c70c3420565ec4ca403d9
Danny Komen <kipropd2@gmail.com>
2022-06-11 14:31:49 -0700

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,5 +7,15 @@
  */
 void print_number(int n)
 {
-	_putchar(n + '0');
+	if (n < 0) {
+        	putchar('-');
+        	n = -n;
+	}
+ 
+    	// Remove the last digit and recur
+    	if (n/10)
+        	print(n/10);
+ 
+    	// Print the last digit
+    	putchar(n%10 + '0');
 }
