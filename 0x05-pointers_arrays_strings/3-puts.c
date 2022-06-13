560b67b1e3a2c17edb1e58dbe9b332aa2343f437
Danny Komen <kipropd2@gmail.com>
2022-06-13 08:58:38 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - printss string
+ *@str: string to be printed
+ *Return: null
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		_putchar(str[i]);
+		str++;
+	}
+}
