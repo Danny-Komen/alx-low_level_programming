0a5fa4677c2512fd59bd3f184951096b21043a90
Danny Komen <kipropd2@gmail.com>
2022-06-13 08:29:24 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - prints the length of the variable
+ * @s: the variable to be tested
+ * Return: integer
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	for(i = 0; *s[i] != '\0'; i++)
+    	{
+        	x++; //Counting the length.
+    	}
+
+	Return x;
+}
