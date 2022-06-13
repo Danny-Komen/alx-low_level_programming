e9f37f3d3c678ea7257b55ef71bc88aeaaf8054f
Danny Komen <kipropd2@gmail.com>
2022-06-13 12:25:31 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print a number of elements from an array
+ * @a: the array
+ * @n: the amount
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (*a = '\0' || i < n)
+	{
+		printf("%d, ", *a);
+		a++;
+		i++;
+	}
+	_putchar('\n');
+}
