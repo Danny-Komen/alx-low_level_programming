5f3fd84ea02700f1df5a893c8c1edecd435856b7
Danny Komen <kipropd2@gmail.com>
2022-06-13 09:38:46 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,27 @@
+#include "main.h"
 
+/**
+ * print_rev - print in reverse order
+ * @s: string to be printed
+ * Return : null
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int x;
+
+	i = 0;
+	while (*s =! '\0')
+	{
+		i++;
+		s++;
+	}
+
+	x = 1;
+	while (x <=i)
+	{
+		_putchar(*(s - x));
+	}
+	_putchar('\n');
+}
