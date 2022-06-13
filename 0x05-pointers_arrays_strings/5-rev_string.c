c589ab7eb6818097625f76bf65444e92bb99dd9f
Danny Komen <kipropd2@gmail.com>
2022-06-13 10:27:21 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string to be reversed
+ * Return: null
+ */
+
+void rev_string(char *s)
+{
+	int w;
+	int q;
+
+	q = s;
+        i = 0;
+        while (*s != '\0')
+        {
+                _putchar(*s);
+                w++;
+                s++;
+        }
+
+	char arr[w];
+	int i;
+
+	for (i = w; i > 0; i--)
+	{
+		arr[i] = q;
+		q++;
+	}
+}	
