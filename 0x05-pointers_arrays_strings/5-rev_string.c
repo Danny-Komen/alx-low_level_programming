2c44a447d4488a37626e672a36134c3989d3cdd8
Danny Komen <kipropd2@gmail.com>
2022-06-13 10:55:35 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -22,7 +22,7 @@ void rev_string(char *s)
 	for (i = 0; i < w/2; i++)  
     	{
 		temp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
+		s[i] = s[w - i - 1];
+		s[w - i - 1] = temp;
 	}
 }
