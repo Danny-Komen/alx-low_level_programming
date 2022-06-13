8e89a4f253432ed3d680748a338def9b3f680ba1
Danny Komen <kipropd2@gmail.com>
2022-06-13 11:35:09 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the string
+ * @str - the string to be printed
+ */
+
+void puts_half(char *str)
+{
+	int size;
+	int n;
+
+	size = 0;
+	while (*str != '\0')
+	{
+		size++;
+		str++;
+	}
+
+	str = str - size;
+	
+	if ((size % 2) == 0)
+	{
+		n = size / 2;	
+	}
+	else
+	{
+		n = (size - 1) / 2;
+	}
+
+	str = str + n;
+
+	while (*str ! = '\0')
+	{
+		_putchar(*str);
+		str++
+	}
+}
