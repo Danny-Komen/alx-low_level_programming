d5e1ca517a9bd6d1832daeb91cadf2c1f0c00ebc
Danny Komen <kipropd2@gmail.com>
2022-06-15 03:20:18 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,49 @@
+# include "main.h"
+
+/**
+ * _strncat - concatnated a limmited number ofcharacters
+ * @dest: destination
+ * @src: source
+ * @n: number of cahracters
+ * Return: a String pointer
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *d;
+	int i;
+	int l;
+
+	i = 0;
+	while (*dest != NULL)
+	{
+		*d = *dest;
+		d++;
+		dest++;
+		i++;
+	}
+
+	*d = " ";
+	d++;
+	i++;
+
+	l = 0;
+	while (*src != NULL)
+	{
+		if (l >= n)
+		{
+			retutn (*d);
+		}
+
+		*d = *scr;
+		d++;
+		scr++;
+		i++;
+		l++;
+	}
+
+	*d = '\0';
+	d = d - i;
+
+	return (*d);
+}
