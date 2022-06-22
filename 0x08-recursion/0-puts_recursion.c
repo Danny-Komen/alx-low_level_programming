a3381fa214fc15d980fc1bcda1571f1dbf3ac3e6
Danny Komen <kipropd2@gmail.com>
2022-06-21 23:42:16 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ * _puts_recursion - prints a sring
+ * @s: the string to be printed
+ * Return: null
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(_puts_recursion(s++));
+	_putchar('\0');
+}
