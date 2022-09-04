d014151bd73d2eadbdb95a5fea0a94ee524f01b4
Danny-Komen <97983723+Danny-Komen@users.noreply.github.com>
2022-09-04 15:00:43 +0300

    first 3

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - return the size of the list
+ * @h: pointer to the start of the list
+ * 
+ * Return: the size of list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    int i;
+
+    for (i = 0; h->n != NULL; i++)
+    {
+
+    }
+    return (i);
+}
